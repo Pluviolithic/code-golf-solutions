53e31370a26b2c6ef01c6911266226850e501f5a
pluviolithic <e-mckanna@onu.edu>
2023-02-22 08:31:49 -0500

    Reduce perl solution by a few chars

--- md5-pepper/pepper.pl
+++ md5-pepper/pepper.pl
@@ -1,1 +1,1 @@
-use Digest::MD5 qw(md5_hex);open(c,'a');@f=split' ',<c>;map{print $_ if md5_hex($f[0].$_)==$f[1]}'A'..'Z','a'..'z';
+use Digest::MD5 'md5_hex';open c,'a';($a,$b)=split' ',<c>;map{print $_ if md5_hex($a.$_)==$b}'A'..'Z','a'..'z'
