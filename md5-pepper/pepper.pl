3c66f38c7d1103f8f3ff2a4f958f83eab8a811f5
pluviolithic <e-mckanna@onu.edu>
2023-02-22 13:08:55 -0500

    Fix bug with perl impl

--- md5-pepper/pepper.pl
+++ md5-pepper/pepper.pl
@@ -1,1 +1,1 @@
-use Digest::MD5 'md5_hex';open c,'a';($a,$b)=split' ',<c>;map{print $_ if md5_hex($a.$_)==$b}'A'..'Z','a'..'z'
+use Digest::MD5 'md5_hex';open c,'a';($a,$b)=split' ',<c>;map{print $_ if md5_hex($a.$_)eq$b}'a'..'z','A'..'Z'
